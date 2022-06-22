cfad6738dfd933ae8b9e2c2635b43610f47f2f30
Caleb Coughenour <CalebCoughenour@protonmail.com>
2022-06-22 11:24:03 -0700

    add changeState function with prop, val & state

--- src/js/script.js
+++ src/js/script.js
@@ -1,1 +1,29 @@
-export
+const hydrate = (plant) => {
+  return {
+    ...plant,
+    water: (plant.water || 0) + 1
+  }
+};
+
+const feed = (plant) => {
+  return {
+    ...plant,
+    soil: (plant.soil || 0) + 1
+  }
+};
+
+const giveLight = (plant) => {
+  return {
+    ...plant,
+    light: (plant.light || 0) + 1
+  }
+};
+
+const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+    [prop]: (state[prop] || 0) + value
+    })
+  }
+};
